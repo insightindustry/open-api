193d64816ee739e383c3c1af211de23c8296899c
insightindustry <software@insightindustry.com>
2019-04-22 06:45:20 -0400

    Implemented utility_functions.parse_json() function.

--- open_api/utility_functions.py
+++ open_api/utility_functions.py
@@ -6,6 +6,14 @@
 # there as needed.
 
 import yaml
+try:
+    import ujson as json
+except ImportError:
+    try:
+        import simplejson as json
+    except ImportError:
+        import json
+
 from validator_collection import validators, checkers
 
 from open_api.errors import DeserializationError
@@ -72,3 +80,71 @@ def parse_yaml(input_data,
             from_yaml = yaml.safe_load(input_file, **kwargs)
 
     return from_yaml
+
+
+def parse_json(input_data,
+               deserialize_function = None,
+               **kwargs):
+    """De-serialize JSON data into a Python :class:`dict <python:dict>` object.
+
+    :param input_data: The JSON data to de-serialize.
+    :type input_data: :class:`str <python:str>`
+
+    :param deserialize_function: Optionally override the default JSON deserializer.
+      Defaults to :obj:`None <python:None>`, which first tries to use
+      :ref:`ujson.loads() <ujson:ujson.loads>`, then falls back to
+      :ref:`simplejson.loads() <simplejson:simplejson.loads>`, and finally defaults
+      to the standard library's :ref:`json.loads() <python:json.loads>`
+      function.
+
+      .. note::
+
+        Use the ``deserialize_function`` parameter to override the default
+        YAML deserializer. A valid ``deserialize_function`` is expected to
+        accept a single :class:`str <python:str>` and return a
+        :class:`dict <python:dict>`, similar to
+        :ref:`simplejson.loads() <simplejson:simplejson.loads>`
+
+        If you wish to pass additional arguments to your ``deserialize_function``
+        pass them as keyword arguments (in ``kwargs``).
+
+    :type deserialize_function: callable / :obj:`None <python:None>`
+
+    :param kwargs: Optional keyword parameters that are passed to the
+      JSON deserializer function. By default, these are options which are passed
+      to :ref:`simplejson.loads() <simplejson:simplejson.loads>`.
+    :type kwargs: keyword arguments
+
+    :returns: A :class:`dict <python:dict>` representation of ``input_data``.
+    :rtype: :class:`dict <python:dict>`
+    """
+    is_file = False
+    if checkers.is_file(input_data):
+        is_file = True
+
+    if deserialize_function is None and not is_file:
+        deserialize_function = json.loads
+    elif deserialize_function is None and is_file:
+        deserialize_function = json.load
+    else:
+        if checkers.is_callable(deserialize_function) is False:
+            raise ValueError(
+                'deserialize_function (%s) is not callable' % deserialize_function
+            )
+
+    if not input_data:
+        raise DeserializationError('input_data is empty')
+
+    if not is_file:
+        try:
+            input_data = validators.string(input_data,
+                                           allow_empty = False)
+        except ValueError:
+            raise DeserializationError('input_data is not a valid string')
+
+        from_json = deserialize_function(input_data, **kwargs)
+    else:
+        with open(input_data, 'r') as input_file:
+            from_json = deserialize_function(input_file, **kwargs)
+
+    return from_json
