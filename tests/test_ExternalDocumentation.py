312794a5c1fea4f09743a333dcd0b1f52f71a42b
Chris Modzelewski <software@insightindustry.com>
2020-08-19 12:09:04 -0400

    Fixed unit tests for ExternalDocumentation class.

--- tests/test_ExternalDocumentation.py
+++ tests/test_ExternalDocumentation.py
@@ -16,7 +16,7 @@ from open_api.utility_classes import Markup, ManagedList, ExternalDocumentation
 
 @pytest.mark.parametrize('value, error', [
     ({'url': 'http://testapi.dev', 'description': 'Test Description'}, None),
-    ({'url': 'invalid-url', 'description': 'Test Description'}, TypeError),
+    ({'url': 'invalid-url', 'description': 'Test Description'}, ValueError),
 ])
 def test_ExternalDocumentation__init__(value, error):
     if not error:
@@ -31,7 +31,7 @@ def test_ExternalDocumentation__init__(value, error):
 
 @pytest.mark.parametrize('value, error', [
     ({'url': 'http://testapi.dev', 'description': 'Test Description'}, None),
-    ({'url': 'invalid-url', 'description': 'Test Description'}, TypeError),
+    ({'url': 'invalid-url', 'description': 'Test Description'}, ValueError),
 ])
 def test_ExternalDocumentation_new_from_dict(value, error):
     if not error:
@@ -48,7 +48,7 @@ def test_ExternalDocumentation_new_from_dict(value, error):
 
 @pytest.mark.parametrize('value, updated_value, error', [
     ({'url': 'http://testapi.dev', 'description': 'Test Description'}, {'description': 'updated description', 'url': 'http://testapi2.dev'}, None),
-    ({'url': 'http://testapi.dev', 'description': 'Test Description'}, {'url': 'invalid-url'}, TypeError),
+    ({'url': 'http://testapi.dev', 'description': 'Test Description'}, {'url': 'invalid-url'}, ValueError),
 ])
 def test_ExternalDocumentation_update_from_dict(value, updated_value, error):
     result = ExternalDocumentation.new_from_dict(value)
